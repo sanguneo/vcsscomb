54ef0476eed84e1078964cc55985459253670964
나상권 <sknah@urbanbase.com>
2019-11-27 09:56:53 +0900

    - initialize

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const csscomb = require('csscomb');
+
+const args = process.argv.slice(2);
+const targetVueFilePath = args[args.length - 1];
+
+const configArgIndex = args.indexOf('--config');
+const comb = new csscomb((configArgIndex !== -1 ) ? args[configArgIndex + 1] : 'csscomb');
+
+const content = fs.readFileSync(targetVueFilePath).toString();
+
+const regExr = /(<style ?(?:lang="(.*)")?>)(.*)?(<\/style>)/gms;
+const main = async () => {
+    const regResult = regExr.exec(content);
+    if (!regResult) {
+        console.log('There is no <style /> in "' + targetVueFilePath + '"');
+        return;
+    }
+    const arrayRes = regResult.slice(1);
+    try {
+        const combResult = await comb.processString(arrayRes[2], {syntax: (arrayRes[1] || 'css')});
+        const endResult = content.replace(regExr, arrayRes[0] + combResult + arrayRes[3]);
+        fs.unlinkSync(targetVueFilePath);
+        fs.writeFileSync(targetVueFilePath, endResult)
+        console.log('CSSComb processed for "' + targetVueFilePath + '"');
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+main();
